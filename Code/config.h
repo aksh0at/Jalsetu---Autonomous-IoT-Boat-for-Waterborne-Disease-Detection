858acb3489971a2f75532f7d00cf9a61dc050e0f
Akshat Srivastava <akshatsri2121@gmail.com>
2026-06-26 11:47:37 +0530

    Create config.h

---
+++ Code/config.h
@@ -0,0 +1,11 @@
+#define PH_PIN 34
+#define TDS_PIN 35
+#define TURBIDITY_PIN 32
+
+#define TRIG_PIN 13
+#define ECHO_PIN 12
+
+#define MOTOR1A 25
+#define MOTOR1B 26
+#define MOTOR2A 27
+#define MOTOR2B 14
